401572a87194a8aef6b9c3a409d61bf904d8ba30
alexdphan <alexphan0515@gmail.com>
2023-01-24 22:07:00 -0500

    pass 5th stage

--- src/main.rs
+++ src/main.rs
@@ -14,11 +14,11 @@ async fn main() -> Result<()> {
     loop {
         let incoming = listener.accept().await;
         match incoming {
-            Ok((stream, _)) => {
+            Ok((mut stream, _)) => {
                 println!("accepted new connection");
 
                 tokio::spawn(async move {
-                    handle_connection(stream).await.unwrap();
+                    handle_connection(&mut stream).await.unwrap();
                 });
             }
             Err(e) => {
@@ -31,7 +31,12 @@ async fn main() -> Result<()> {
 // TcpStream is a wrapper around a socket, used to read and write data (argument)
 // async function that returns a Result
 async fn handle_connection(stream: &mut TcpStream) -> Result<()> {
-    let mut buf = [0, 512];
+    // create a buffer to store the data we read from the socket
+    // BytesMut is a type from the bytes crate that provides a mutable buffer of bytes
+    // represent a unique viet into a potentially shared memory region
+    // owners of BytesMut handles can mutate the buffer without affecting other owners
+    let mut buf = BytesMut::with_capacity(512);
+    // It is similar to a Vec<u8> but with less copies and allocations.
 
     // read and write calls are await ed
     // These changes allow Tokio to suspend and resume our connection handler at the right times, and do work on tasks for other clients while ours is suspended.
