a53f050ce4fabc94f55889279bedcad7c473d719
alexdphan <alexphan0515@gmail.com>
2023-01-28 22:51:01 -0500

    pass 6th stage

--- src/main.rs
+++ src/main.rs
@@ -1,14 +1,9 @@
-// Result is a type alias for Result<T, E> where E is always anyhow::Error
 use anyhow::Result;
-// BytesMut is a type from the bytes crate that provides a mutable buffer of bytes
-use bytes::BytesMut;
-// AsyncReadExt and AsyncWriteExt are traits that provide read_buf() and write() methods
-use tokio::io::{AsyncReadExt, AsyncWriteExt};
-// TcpListener and TcpStream are types from the tokio crate that provide methods for listening for and accepting connections, and reading and writing data
+// use bytes::BytesMut;
+// use tokio::io::{AsyncReadExt, AsyncWriteExt};
+use crate::resp::Value::{Error, SimpleString};
 use tokio::net::{TcpListener, TcpStream};
-// provides RespConnection, a type that wraps a TcpStream and provides methods for reading and writing RESP values
-use resp::Value::{Error, SimpleString};
-// mod resp is a module that provides a type for reading and writing RESP values
+
 mod resp;
 
 // to let Tokio start a runtime before our main function does any work.
@@ -38,16 +33,15 @@ async fn main() -> Result<()> {
 // TcpStream is a wrapper around a socket, used to read and write data (argument)
 // async function that returns a Result
 async fn handle_connection(stream: TcpStream) -> Result<()> {
-    // resp provides a simple RespConnection, which wraps a TcpStream and provides methods for reading and writing RESP values
     let mut conn = resp::RespConnection::new(stream);
 
-    // we assume the incoming message will be represented as a RESP array (Value::Array) and use a utility function to get the first element of the array (head element), the command, the tail of the array (tail elements), and the arguments
-    // check command to see if it was a PING or an ECHO. If it was a PING, we respond with a simple string, "PONG". If it was an ECHO, we respond with the first argument
-    // if the command was not a PING or an ECHO (otherwise), we respond with an error
     loop {
+        // Wait for the client to send us a message but ignore the content for now
+        // let bytes_read = stream.read_buf(&mut buf).await?;
+        // if bytes_read == 0 {
+        //     println!("client closed the connection");
         let value = conn.read_value().await?;
 
-        // if there is a value, we unwrap it and call to_command() to get the command and arguments
         if let Some(value) = value {
             let (command, args) = value.to_command()?;
             let response = match command.to_ascii_lowercase().as_ref() {
@@ -55,10 +49,13 @@ async fn handle_connection(stream: TcpStream) -> Result<()> {
                 "echo" => args.first().unwrap().clone(),
                 _ => Error(format!("command not implemented: {}", command)),
             };
+
             conn.write_value(response).await?;
         } else {
             break;
         }
+
+        // stream.write("+PONG\r\n".as_bytes()).await?;
     }
 
     Ok(())
